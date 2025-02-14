cb3b3bfa5d509fab62baa439399133ddfcb41799
Rohn Roy <rohnroy007@gmail.come>
2025-02-14 22:00:25 +0530

    updated vite config ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Fashion-page-sample/', // 🔥 Add your GitHub repo name here!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
